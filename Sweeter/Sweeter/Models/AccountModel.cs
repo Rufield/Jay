c3dbc4a4b672b757041c991ec31c88ffc55e44e7
schect404 <tomilovych@dlit.dp.ua>
2018-01-24 17:56:01 +0200

    Update AccountModel.cs

--- Sweeter/Sweeter/Models/AccountModel.cs
+++ Sweeter/Sweeter/Models/AccountModel.cs
@@ -12,6 +12,7 @@ namespace Sweeter.Models
         public string Email { get; set; }
         public string FullName { get; set; }
         public string Avatar { get; set; }
+        public string Password {get;set;}
         public List<NewsModel> MyPosts { get; set; }
     }
 }
