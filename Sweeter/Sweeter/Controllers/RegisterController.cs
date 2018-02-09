79c2acb6d3fbf78a836fa146170b2b39c61a635e
Catzoy <demchenko20021@gmail.com>
2018-02-09 11:01:58 +0200

    Registration
    Добавлены скрипты для первоначальной проверки введённого. Изменён метод валидации.

--- Sweeter/Sweeter/Controllers/RegisterController.cs
+++ Sweeter/Sweeter/Controllers/RegisterController.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Sweeter.DataProviders;
 using Sweeter.Models;
 using System.Security.Cryptography;
 using System.Text;
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 
 namespace Sweeter.Controllers
 {
+   
     [Route("/Register")]
     public class RegisterController : Controller
     {
@@ -27,98 +27,61 @@ namespace Sweeter.Controllers
         }
         string GetHashString(string s)
         {
-            
             byte[] bytes = Encoding.Unicode.GetBytes(s);
-
-            
             MD5CryptoServiceProvider CSP =
                 new MD5CryptoServiceProvider();
-
-             
             byte[] byteHash = CSP.ComputeHash(bytes);
-
             string hash = string.Empty;
-
-             
             foreach (byte b in byteHash)
                 hash += string.Format("{0:x2}", b);
-
             return hash;
         }
+
         [HttpPost]
-        public IActionResult Form(string avatar,string fullname, string username, string Email, string password, string password2)
+        public IActionResult Index(AccountModel account, IFormFile avatar)
         {
-      
-            
-                if (accountDataProvider.GetAccountsByEmail(Email).Count() == 0)
+            if (ModelState.IsValid)
+            {
+                if (accountDataProvider.GetAccountsByEmail(account.Email).Count() == 0)
                 {
-                if (accountDataProvider.GetAccountsByUsername(username).Count() == 0)
-                {
-                    if (password.Equals(password2))
+                    if (accountDataProvider.GetAccountsByUsername(account.Username).Count() == 0)
                     {
-                        AccountModel account = new AccountModel
+                        if (avatar != null)
                         {
-                            
-                            Name=fullname,
-                            Username = username,
-                            Email = Email,
-                            Password = GetHashString(password),
-                            Avatar = avatar
-
-
-
-                        };
+                            byte[] ImageData;
+                            var filepath = Path.GetTempFileName();
+                            using (Stream fs = avatar.OpenReadStream())
+                            {
+                                ImageData = new byte[fs.Length];
+                                fs.Read(ImageData, 0, ImageData.Length);
+                            }
+                            account.Avatar = ImageData;
+                        }
+                        account.Password = GetHashString(account.Password);
                         accountDataProvider.AddAccount(account);
                         return RedirectToAction("Index", "Login");
                     }
                     else
                     {
-                        ViewData["UsernameStat"] = "";
-                        ViewData["Name"] = fullname;
-                        ViewData["Username"] = username;
-                        ViewData["Email"] = Email;
-
-
-                        ViewData["EmailStat"] = "";
-                        ViewData["PasswordStat"] = "notright";
-                        ViewData["ErrorMessage"] = "Passwords are not equal";
+                        SetBack();
+                        ViewData["Error"] = "This Username is already in use.";
                         return View();
                     }
-
                 }
                 else
                 {
-                    ViewData["UsernameStat"] = "notright";
-                    ViewData["Name"] = fullname;
-                    ViewData["Username"] = "";
-                    ViewData["Email"] = Email;
-
-
-                    ViewData["EmailStat"] = "";
-                    ViewData["PasswordStat"] = "";
-                    ViewData["ErrorMessage"] = "Such username already exists";
+                    SetBack();
+                    ViewData["Error"] = "This Email is already in use.";
                     return View();
                 }
-                }
-                else
-                {
-                    ViewData["UsernameStat"] = "";
-                    ViewData["Name"] = fullname;
-                    ViewData["Username"] = username;
-                    ViewData["Email"] = "";
-                 
-
-                    ViewData["EmailStat"] = "notright";
-                    ViewData["PasswordStat"] = "";
-                    ViewData["ErrorMessage"] = "Such email already exists";
-                    return View();
-                }
-                
             }
-            
+            else return View();
+            void SetBack()
+            {
+                ViewData["Name"] = account.Name;
+                ViewData["Username"] = account.Username;
+                ViewData["Email"] = account.Email;
+            }
         }
-      
-
-
     }
-
+}
---
+++ Sweeter/Sweeter/wwwroot/js/RegisterPageScripts.js
@@ -0,0 +1,42 @@
+﻿function upload() {
+	var preview = document.querySelector('img');
+	var file = document.querySelector('input[type=file]').files[0];
+	var reader = new FileReader();
+
+	reader.onloadend = function () { preview.src = reader.result; }
+
+	if (file) {
+		reader.readAsDataURL(file);
+	} else { preview.src = "~/lib/img/avatar.jpeg" }
+}
+
+function check() {
+	var pas1 = document.getElementById('password');
+	var pas2 = document.getElementById('password2');
+	var ermes = document.getElementById('error');
+	var button = document.getElementById('register');
+	if (pas1.value != pas2.value) {
+		pas1.classList.add("notright");
+		pas2.classList.add("notright");
+		ermes.innerHTML = "Passwords are not simmilar!";
+		button.disabled = true;
+	}
+	else {
+		pas1.classList.remove("notright");
+		pas2.classList.remove("notright");
+		ermes.innerHTML = "";
+		button.disabled = false;
+	}
+}
+
+function clearerror() {
+	var ermes = document.getElementById('error');
+	ermes.innerHTML = "";
+}
+
+function checkemail() {
+	var email = document.getElementById('email').value;
+	var ermes = document.getElementById('error');
+	if (email.indexOf(".", email.indexOf("@"))==-1) ermes.innerHTML = "Email is invalid!";
+	else ermes.innerHTML = "";
+}
