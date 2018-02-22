5589826b27b30c7e7eca8d6125a72ea82449cbed
schect404 <tomilovych@dlit.dp.ua>
2018-02-22 20:16:46 +0200

    Added HideButton

--- Sweeter/Sweeter/wwwroot/js/site.js
+++ Sweeter/Sweeter/wwwroot/js/site.js
@@ -1,1 +1,17 @@
 ﻿// Write your Javascript code.
+window.onload = function () {
+    var hide = document.getElementById('HideForm');
+    var show = document.getElementById('ShowForm');
+    var form = document.getElementById('Addfeed');
+
+    hide.onclick = function () {
+        form.classList.add('hidden');
+        hide.classList.add('hidden');
+        show.classList.remove('hidden');
+    }
+    show.onclick = function () {
+        form.classList.remove('hidden');
+        hide.classList.remove('hidden');
+        show.classList.add('hidden');
+    }
+}
